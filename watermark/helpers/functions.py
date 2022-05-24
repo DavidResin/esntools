5a78ea3897154e8fcfd57155cba1a33cb180712e
davidresin <davidresin@citycable.ch>
2022-05-24 03:47:00 +0200

    Progress

---
+++ watermark/helpers/functions.py
@@ -0,0 +1,241 @@
+##################################################
+##												##
+##   File : 	functions.py 					##
+##   Author : 	David Resin, ESN EPFL 			##
+##   Date :   	24.05.2022						##
+##   Email :  	davidresin@citycable.ch 		##
+##   Repo :   	github.com/DavidResin/esntools	##
+##												##
+## ============================================ ##
+##												##
+##	 THIS IS A HELPER FILE AND IS NOT MEANT		##
+##	 TO BE RUN ON ITS OWN.						##
+##												##
+##################################################
+
+# Default libraries
+import argparse
+import os
+import random
+import shutil
+import sys
+import textwrap
+
+# External library
+from PIL import ImageColor, ImageDraw, ExifTags
+
+# Color parsing
+def parse_color(args, color_choices):
+	color = args["color"]
+
+	try:
+		if color == "random":
+			ret = []
+		elif color == "all":
+			ret = list(color_choices.values())
+		else:
+			ret = [color_choices.get(color) or ImageColor.getrgb(color)]
+	except ValueError:
+		sys.exit("Wrong color format. Official ESN color or #rrggbb hexadecimal format expected.")
+
+	return ret
+	
+# Create a directory if it is missing
+def create_dir_if_missing(path):
+	if not os.path.exists(path):
+		os.makedirs(path)
+
+# Auromatically tilt an image based on its EXIF data
+def tilt_img(img, tilt_map):
+	tilt = dict((ExifTags.TAGS[k], v) for k, v in img._getexif().items() if k in ExifTags.TAGS).get('Orientation')
+	tilt_idx = tilt_map[(tilt - 1) // 2]
+	return img.rotate(tilt_idx, expand=True)
+
+# Flush the output directory
+def flush_output(path_out, exts):
+	for old in os.listdir(path_out):
+		if any([old.lower().endswith(ext) for ext in exts]):
+			os.remove(os.path.join(path_out, old))
+
+# Move an invalid picture out
+def invalidate(fn, img_path, path_inv, invalid_count):
+	shutil.move(img_path, os.path.join(path_inv, fn))
+	return invalid_count + 1
+
+# Generate list of positions based on arguments
+def get_pos_list(pos):
+	if pos == "random":
+		ret = [pos_choices[random.randint(0, 3)]]
+	elif pos == "all":
+		ret = pos_choices[:4]
+	else:
+		ret = [pos]
+
+	return ret
+
+# Watermark an image with a given position and color
+def watermark_image_pos_color(img, sub_img, big_sub_img, file_out, pos, logo, crop_edges, circle_edges, circle_center, circle_radius, logo_dims, logo_center, color, shifts, ss_factor, draw_circle=True, suffix=""):
+	# Drawing the circle if requested
+	if draw_circle:
+		ImageDraw.Draw(big_sub_img).ellipse([ss_factor * elem for elem in circle_edges], fill=color)
+
+	# Pasting the logo
+	big_sub_img.paste(logo, [int(ss_factor * (edges + circle_radius - shift) - dim / 2) for edges, shift, dim in zip(circle_edges[:2], shifts, logo.size)], logo)
+
+	# Downscale the watermark area back to its normal size then paste it on the full image
+	sub_img = big_sub_img.resize(sub_img.size)
+	img_out = img.copy()
+	img_out.paste(sub_img, crop_edges[:2])
+
+	# Format suffix
+	if len(suffix):
+		suffix = "_" + suffix
+
+	# Save the image
+	fst, snd = file_out
+	img_out.save(fst + "_" + pos + str(suffix) + "." + snd)
+
+# Watermark an image with a given position and a list of colors
+def watermark_image_pos(img, file_out, pos, logos, logo_dims, logo_center, circle_center, circle_radius, shifts, color_list, ss_factor, draw_circle=True):
+	# Compute circle bounding box from the center and radius
+	circle_edges_x = circle_center[0] - circle_radius, circle_center[0] + circle_radius
+	circle_edges_y = circle_center[1] - circle_radius, circle_center[1] + circle_radius
+
+	# Compute real bounding box by removing out-of-bounds parts
+	crop_edges_x = max(0, circle_edges_x[0]), min(img.size[0], circle_edges_x[1])
+	crop_edges_y = max(0, circle_edges_y[0]), min(img.size[1], circle_edges_y[1])
+	crop_edges = tuple([int(elem) for elem in [crop_edges_x[0], crop_edges_y[0], crop_edges_x[1], crop_edges_y[1]]])
+
+	# Compute circle edges relative to the cropped area
+	padded_circle_edges_x = [elem - crop_edges_x[0] for elem in circle_edges_x]
+	padded_circle_edges_y = [elem - crop_edges_y[0] for elem in circle_edges_y]
+	padded_circle_edges = tuple([int(elem) for elem in [padded_circle_edges_x[0], padded_circle_edges_y[0], padded_circle_edges_x[1], padded_circle_edges_y[1]]])
+
+	# Crop out the area of the watermark and upscale it
+	sub_img = img.crop(box=crop_edges)
+	big_sub_img = sub_img.resize([ss_factor * dim for dim in sub_img.size])
+		
+	# Loop through the selected colors
+	for suffix, color in enumerate(color_list):
+		#variation_count += 1
+		logo = logos["color"] if color == (255, 255, 255) else logos["white"]	
+			
+		# No need for color suffix if only one color
+		if len(color_list) == 1:
+			suffix = ""
+
+		# Watermark picture
+		watermark_image_pos_color(	img=img, \
+									sub_img=sub_img, \
+									big_sub_img=big_sub_img, \
+									file_out=file_out, \
+									pos=pos, \
+									logo=logo, \
+									crop_edges=crop_edges, \
+									circle_edges=padded_circle_edges, \
+									circle_center=circle_center, \
+									circle_radius=circle_radius, \
+									logo_dims=logo_dims, \
+									logo_center=logo_center, \
+									color=color, \
+									shifts=shifts, \
+									ss_factor=ss_factor, \
+									draw_circle=draw_circle, \
+									suffix=str(suffix))
+
+# Watermark an image with a list of positions and a list of colors
+def watermark_image(img, file_out, logos, ratios, pos_list, color_list, draw_circle=True, center_circle=False):
+	image_width, image_height = img.size
+	src_logo_width, src_logo_height = logos["color"].size
+
+	# Compute logo dimensions from image dimensions and image-watermark ratio
+	tgt_logo_height = ratios["img_wm_ratio"] * image_height
+	tgt_logo_width = tgt_logo_height / src_logo_height * src_logo_width
+
+	# Rescale the logos for the current picture
+	logos_scaled = dict([(key, logo.resize([int(ratios["ss_factor"] * elem) for elem in [tgt_logo_width, tgt_logo_height]])) for key, logo in logos.items()])
+
+	# Compute logo padding from padding ratio and logo height
+	logo_padding = ratios["logo_padding_ratio"] * tgt_logo_height
+
+	# Compute logo center from padding and logo dimensions
+	logo_center_x = logo_padding + tgt_logo_width / 2
+	logo_center_y = logo_padding + tgt_logo_height / 2
+
+	# Iterate through positions
+	for pos in pos_list:
+		# Set logo center depending on the chosen position
+		curr_logo_center_x = logo_center_x if "left" in pos else image_width - logo_center_x
+		curr_logo_center_y = logo_center_y if "top" in pos else image_height - logo_center_y
+
+		# Set circle position
+		ratio_x = .5 if center_circle else ratios["circle_shift_ratio_x"]
+		ratio_y = .5 if center_circle else ratios["circle_shift_ratio_y"]
+
+		# Compute shifts
+		sX = tgt_logo_width * (ratio_x - .5) * ((-1) ** ("left" in pos))
+		sY = tgt_logo_height * (ratio_y - .5) * ((-1) ** ("top" in pos))
+
+		# Compute circle center from logo center and offset factors
+		curr_circle_center_x = curr_logo_center_x + sX
+		curr_circle_center_y = curr_logo_center_y + sY
+
+		# Compute circle radius from logo width and logo-circle ratio
+		circle_radius = tgt_logo_width * ratios["logo_circle_ratio"] / 2
+
+		watermark_image_pos(img=img, \
+							file_out=file_out, \
+							pos=pos, \
+							logos=logos_scaled, \
+							logo_dims=[tgt_logo_width, tgt_logo_height], \
+							logo_center=[curr_logo_center_x, curr_logo_center_y], \
+							circle_center=[curr_circle_center_x, curr_circle_center_y], \
+							circle_radius=circle_radius, \
+							shifts=[sX, sY], \
+							color_list=color_list, \
+							ss_factor=ratios["ss_factor"], \
+							draw_circle=draw_circle)
+
+# Setup argument parser
+def setup_argparser():
+	ap = argparse.ArgumentParser(description="ESN Lausanne Watermark Inserter", formatter_class=argparse.RawTextHelpFormatter)
+	ap.add_argument("-f",	"--flush",				action="store_true",												help="flush output folder")
+	ap.add_argument("-k",	"--keep",				action="store_true",												help=f"keep original file format (if omitted, will default to { default_format.upper() } format)")
+	ap.add_argument("-np",	"--no-prefix",			action="store_true",												help=f"do not add a '{ wm_prefix }' prefix to outputs")
+	ap.add_argument("-nr",	"--no-rotate",			action="store_true",												help=f"do not rotate images if they are not upright")
+	ap.add_argument("-nc",	"--no-circle",			action="store_true",												help="do not add a colored circle behind the logo (not recommended)")
+	ap.add_argument("-cc",	"--center-circle",		action="store_true",												help="center the circle around the logo (not recommended)")
+	ap.add_argument("-i",	"--input",				action="store", type=str,	default=default_input,					help=f"set a custom input directory path (default is '{ default_input }')")
+	ap.add_argument("-o",	"--output",				action="store", type=str,	default=default_output,					help=f"set a custom output directory path (default is '{ default_output }')")
+	ap.add_argument("-wms",	"--watermark-size",		action="store", type=float,	default=default_wm_size,				help=f"set the size of the watermark compared to the image's size (default is { default_wm_size })")
+	ap.add_argument("-wmr",	"--watermark-ratio",	action="store", type=float,	default=default_wm_ratio,				help=f"set the size ratio between the logo's width and the circle's diameter, (default is { default_wm_ratio })")
+	ap.add_argument("-wmp",	"--watermark-padding",	action="store", type=float,	default=default_wm_padding,				help=f"set the padding between the logo and the edge of the picture, as a ratio of the logo's height (default is { default_wm_padding })")
+	ap.add_argument("-ss",  "--supersampling",		action="store", type=int,	default=default_ss, metavar="FACTOR",	help=f"set the supersampling factor for smoothing the circle (default is { default_ss }, smaller means faster execution but less smoothing)")
+	ap.add_argument("-c",	"--color",				action="store",
+													type=str,
+													default=list(color_choices.keys())[0],
+													help=textwrap.dedent("set the color of the circle, options are the following:\n"
+																		+ "> official ESN colors:\n"
+																			+ "\t'white' [Default value]\n"
+																			+ "\t'black'\n"
+																			+ "\t'magenta'\n"
+																			+ "\t'orange'\n"
+																			+ "\t'green'\n"
+																			+ "\t'cyan'\n"
+																			+ "\t'purple'\n"
+																		+ "> 'random' [Random color for each image]\n"
+																		+ "> 'all' [All versions of each image with suffixes]\n"
+																		+ "> '#rrggbb' [Any other HEX RGB color, not recommended]"))
+	ap.add_argument("-p",	"--position",			type=str,
+													metavar="POSITION",
+													default=pos_choices[0],
+													choices=pos_choices,
+													help=textwrap.dedent("set the position of the watermark, options are the following:\n"
+																		+ "> 'bottom_right' [Default value]\n"
+																		+ "> 'bottom_left'\n"
+																		+ "> 'top_right'\n"
+																		+ "> 'top_left'\n"
+																		+ "> 'random' [Random edge for each image]\n"
+																		+ "> 'all' [All versions of each image with suffixes]"))
+
+	return ap
--- watermark/watermark.py
+++ watermark/watermark.py
@@ -1,245 +1,162 @@
-import argparse, math, os, random, shutil, sys, textwrap
-from PIL import Image, ImageColor, ImageDraw, ExifTags
+##################################################
+##												##
+##   File : 	watermark.py 					##
+##   Author : 	David Resin, ESN EPFL 			##
+##   Date :   	24.05.2022						##
+##   Email :  	davidresin@citycable.ch 		##
+##   Repo :   	github.com/DavidResin/esntools	##
+##												##
+## ============================================ ##
+##												##
+##	 HOW TO RUN									##
+##		1 - Install Python 3					##
+##		2 - "cd path/to/the/watermark/folder"	##
+##		3 - "pip install -r requirements.txt"	##
+##		4 - "python watermark.py"				##
+##												##
+##################################################
 
-# Define image exporting routine
-def export_image(img, big_img, factor, wm, pos, color, dist, file_out, circle, radius, suff=""):
-	p = "".join([x[0] for x in pos.split("-")])
-	iW, iH = img.size
-	wW, wH = wm.size
+# Default libraries
+import os
+import random
+import sys
 
-	# Computing logo position
-	sH = wH / 2 + min(wH, wW) * dist
-	sH = int(sH if "t" in p else iH / 2 if p == "c" else iH - sH)
+# External libraries
+from PIL import Image, UnidentifiedImageError
+import pillow_heif
+
+# Custom library
+from helpers import *
+
+# Main code
+if __name__ == "__main__":
+
+	# Define default values
+	default_wm_size = 1 / 8
+	default_wm_ratio = 1.6
+	default_wm_padding = 0.15
+	default_ss = 8
+	default_input = "input"
+	default_output = "output"
+	default_format = "png"
+
+	# Other parameters
+	exts = ('.jpg', '.png', '.jpeg', '.ico', '.webp', '.heic')
+	preproc_exts = ('.heic')
+	proc_str = "Processing image {} of {} \t({} of {} variations, \tinvalid: {})"
+	end_str = "Processed {} images successfully!"
+	inv_str = " ({} image(s) failed and moved to 'invalid')"
+	wm_prefix = "wm_"
+	pos_choices = ["bottom_right", "bottom_left", "top_right", "top_left", "random", "all"]
+	path_inv = "invalid"
+	tilt_map = {0: 0, 1: 180, 2: 270, 3: 90}
+	color_choices = {
+		"white": (255, 255, 255),
+		"black": (0, 0, 0),
+		"magenta": (236, 0, 140),
+		"orange": (244, 123, 32),
+		"green": (122, 193, 67),
+		"cyan": (0, 174, 239),
+		"purple": (46, 49, 146),
+	}
+
+	# Names of logo files
+	fn_color = "logo_color.png"
+	fn_white = "logo_white.png"
+
+	# Open logo files
+	logos = {
+		"color": Image.open(os.path.join("logos", fn_color)),
+		"white": Image.open(os.path.join("logos", fn_white))
+	}
+
+	# Setup argparser and parse arguments
+	ap = setup_argparser()
+	args = vars(ap.parse_args())
+
+	prefix = 	wm_prefix * (not args["no_prefix"])
+	path_in = 	args["input"]
+	path_out = 	args["output"]
+	pos = 		args["position"]
 	
-	sW = wW / 2 + min(wH, wW) * dist
-	sW = int(sW if "l" in p else iW / 2 if p == "c" else iW - sW)
-		
-	# Drawint the circle
-	if circle:
-		r = int(max(wW, wH) * radius / 2)
-		ImageDraw.Draw(big_img).ellipse(tuple(factor * x for x in (sW - r, sH - r, sW + r, sH + r)), fill=color)
-	
-	# Downsample
-	img = big_img.resize(img.size)
+	ratios = {
+		"img_wm_ratio": 		args["watermark_size"],
+		"logo_padding_ratio": 	args["watermark_padding"],
+		"logo_circle_ratio": 	args["watermark_ratio"],
+		"circle_shift_ratio_x": 3 / 5,
+		"circle_shift_ratio_y": 1,
+		"ss_factor": 			args["supersampling"]
+	}
 
-	# Pasting the logo
-	img.paste(wm, (sW - int(wW / 2), sH - int(wH / 2)), wm)
-	
-	# Saving
-	fst, snd = file_out
-	img.save(fst + "_" + p + str(suff) + "." + snd)
+	# Create missing folders if needed
+	create_dir_if_missing(path_out)
+	create_dir_if_missing(path_inv)
 
-# Define default data
-d_size = 0.025
-d_dist = .3
-d_radius = 1.55
-d_ss = 8
-d_input = "input"
-d_output = "output"
-d_format = "png"
-fn_color = "logo_color.png"
-fn_white = "logo_white.png"
-prefix = "wm_"
-pos_choices = ["bottom-right", "bottom-left", "top-right", "top-left", "center", "random", "random-plus", "all"]
-color_choices = {
-	"white": (255, 255, 255),
-	"black": (0, 0, 0),
-	"magenta": (236, 0, 140),
-	"orange": (244, 123, 32),
-	"green": (122, 193, 67),
-	"cyan": (0, 174, 239),
-	"purple": (46, 49, 146),
-}
+	# Parse color
+	color_list = parse_color(args, color_choices)
 
-# Setup argument parser
-ap = argparse.ArgumentParser(description="ESN Lausanne Watermark Inserter", formatter_class=argparse.RawTextHelpFormatter)
-ap.add_argument("-q",	"--quiet",			action="store_true",										help="silent running")
-ap.add_argument("-f",	"--flush",			action="store_true",										help="flush output folder")
-ap.add_argument("-k",	"--keep",			action="store_true",										help=f"keep original file format (if omitted, will default to { d_format.upper() } format)")
-ap.add_argument("-np",	"--no-prefix",		action="store_true",										help=f"do not add a '{ prefix }' prefix to outputs")
-ap.add_argument("-nr",	"--no-rotate",		action="store_true",										help=f"do not rotate images if they are not upright")
-ap.add_argument("-nc",	"--no-circle",		action="store_true",										help="do not add a colored circle behind the logo (not recommended)")
-ap.add_argument("-i",	"--input",			action="store", type=str,	default=d_input,				help=f"set a custom input directory path (default is '{ d_input }')")
-ap.add_argument("-o",	"--output",			action="store", type=str,	default=d_output,				help=f"set a custom output directory path (default is '{ d_output }')")
-ap.add_argument("-s",	"--size",			action="store", type=float,	default=d_size,					help=f"set the ratio of the watermark's size compared to the image's size (default is { d_size })")
-ap.add_argument("-d",	"--distance",		action="store", type=float,	default=d_dist,					help=f"set the ratio of the watermark's padding to the size of the watermark (default is { d_dist })")
-ap.add_argument("-r",	"--radius",			action="store", type=float,	default=d_radius,				help=f"set the ratio of the circle's size compared to the logo's size (default is { d_radius })")
-ap.add_argument("-ss",  "--supersampling",	action="store", type=int,	default=d_ss, metavar="FACTOR",	help=f"set the supersampling factor for smoothing the circle (default is { d_ss }, smaller means faster execution but less smoothing)")
-ap.add_argument("-c",	"--color",			action="store",
-											type=str,
-											default=list(color_choices.keys())[0],
-											help=textwrap.dedent("set the color of the circle, options are the following:\n"
-															+ "> official ESN colors:\n"
-																+ "\t'white' [Default value]\n"
-																+ "\t'black'\n"
-																+ "\t'magenta'\n"
-																+ "\t'orange'\n"
-																+ "\t'green'\n"
-																+ "\t'cyan'\n"
-																+ "\t'purple'\n"
-															+ "> 'random' [Random color for each image]\n"
-															+ "> 'all' [All versions of each image with suffixes]\n"
-															+ "> '#rrggbb' [Any other HEX RGB color, not recommended]"))
-ap.add_argument("-p",	"--position",	type=str,
-										metavar="POSITION",
-										default=pos_choices[0],
-										choices=pos_choices,
-										help=textwrap.dedent("set the position of the watermark, options are the following:\n"
-															+ "> 'bottom-right' [Default value]\n"
-															+ "> 'bottom-left'\n"
-															+ "> 'top-right'\n"
-															+ "> 'top-left'\n"
-															+ "> 'center'\n"
-															+ "> 'random' [Random edge for each image]\n"
-															+ "> 'random-plus' [Random edge or center for each image]\n"
-															+ "> 'all' [All versions of each image with suffixes]"))
-
-# Parse arguments
-args = vars(ap.parse_args())
-
-quiet = args["quiet"]
-flush = args["flush"]
-keep = args["keep"]
-prefix = prefix * (not args["no_prefix"])
-rotate = not args["no_rotate"]
-circle = not args["no_circle"]
-path_in = args["input"]
-path_out = args["output"]
-path_inv = "invalid"
-ratio = args["size"]
-dist = args["distance"]
-radius = args["radius"]
-color = args["color"]
-pos = args["position"]
-ss = args["supersampling"]
-
-# Parse color
-try:
-	if color == "random":
-		this_color = []
-	elif color == "all":
-		this_color = list(color_choices.values())
-	else:
-		this_color = [color_choices.get(color) or ImageColor.getrgb(color)]
-except:
-	sys.exit("Wrong color format. Official ESN color or #rrggbb hexadecimal format expected.")
-
-# Define work variables
-try:
-	fns = [el for el in os.listdir(path_in) if el != ".gitkeep"]
-	this_pos = pos
-	count, total, invalid = 0, len(fns), 0
-except:
-	if not os.path.exists(d_input):
-		os.makedirs(d_input)
-		
-	sys.exit("Input folder not found. Make sure you arguments are correct or use the default '" + d_input + "' folder.")
-	
-# Create missing folders
-if not os.path.exists(path_out):
-	os.makedirs(path_out)
-	
-if not os.path.exists(path_inv):
-	os.makedirs(path_inv)
-
-exts = ('.jpg', '.png', '.jpeg', '.ico', '.webp')
-proc_str = "Processing image {} of {} \t({} of {} variations, \tinvalid: {})"
-end_str = "Processed {} images successfully!"
-inv_str = " ({} image(s) failed and moved to 'invalid')"
-
-# Load watermark
-wm_color = Image.open(fn_color)
-wm_white = Image.open(fn_white)
-wm = wm_color
-wW, wH = wm.size
-
-# Flush all valid images in output directory if asked to
-if flush:
-	for old in os.listdir(path_out):
-		if any([old.lower().endswith(ext) for ext in exts]):
-			os.remove(os.path.join(path_out, old))
-
-# Apply to all images
-for fn in fns:
-	count += 1
-	
-	# Generate paths
-	file_in = os.path.join(path_in, fn)
-	file_out = os.path.join(path_out, prefix + fn).rsplit(".", 1)
-	
-	# Move picture to 'invalid' if it doesn't have the right file format
-	if not any([fn.lower().endswith(ext) for ext in exts]):
-		invalid += 1
-		shutil.move(file_in, os.path.join(path_inv, fn))
-		continue
-		
-	# Try to load image
+	# Process filenames
 	try:
-		img = Image.open(file_in)
-		iW, iH = img.size
-		wm_ratio = (wW * wH) / (iW * iH)
+		fns = [el for el in os.listdir(path_in) if el != ".gitkeep"]
 	except:
-		invalid += 1
-		shutil.move(file_in, os.path.join(path_inv, fn))
-		continue
+		create_dir_if_missing(default_input)
+		sys.exit("Input folder not found. Make sure you arguments are correct or use the default '" + default_input + "' folder.")
+
+	img_total = len(fns)
+
+	# Flush all images in the output directory if asked to
+	if args["flush"]:
+		flush_output(path_out, exts)
+		
+	# Apply to all images
+	processed_count = 0
+	invalid_count = 0
+
+	for fn in fns:
+		processed_count += 1
+		
+		# Generate paths
+		file_in = os.path.join(path_in, fn)
+		file_out = os.path.join(path_out, prefix + fn).rsplit(".", 1)
+		
+		# Move picture to 'invalid' if it doesn't have the right file format
+		if not any([fn.lower().endswith(ext) for ext in exts]):
+			invalid_count = invalidate(fn, file_in, path_inv, invalid_count)
+			continue
 	
-	# Unify format if asked to
-	if keep:
-		file_out[1] = d_format
-
-	# Try to re-orient the picture if it is allowed and orientation data is available 
-	if rotate and img._getexif():
-		tilt = dict((ExifTags.TAGS[k], v) for k, v in img._getexif().items() if k in ExifTags.TAGS).get('Orientation')
+		# Try to load image
+		if any([fn.lower().endswith(ext) for ext in preproc_exts]):
+			# Special formats
+			heif_file = pillow_heif.read_heif(file_in)
+			img = Image.frombytes(heif_file.mode, heif_file.size, heif_file.data, "raw")
+			fn = fn.rsplit(".", maxsplit=1)[0] + "." + default_format
+		else:
+			try:
+				img = Image.open(file_in)
+			except (FileNotFoundError, UnidentifiedImageError, ValueError) as e:
+				invalid_count = invalidate(fn, file_in, path_inv, invalid_count)
+				continue
 		
-		try:  
-			if tilt in [3, 4]:
-				img = img.rotate(180, expand=True)
-			elif tilt in [5, 6]:
-				img = img.rotate(270, expand=True)
-			elif tilt in [7, 8]:
-				img = img.rotate(90, expand=True)
-		except:
-			pass
-	
-	# Choose a color for the 'random' case
-	if color == "random":
-		this_color = [random.choice(list(color_choices.values()))]
+		# Unify format if asked to
+		if args["keep"]:
+			file_out[1] = default_format
+
+		# Try to re-orient the picture if it is allowed and orientation data is available 
+		if not args["no_rotate"] and img._getexif():
+			img = tilt_img(img, tilt_map)
 		
-	# Randomize position if asked
-	if pos == "random":
-		this_pos = [pos_choices[random.randint(0, 3)]]
-	elif pos == "random-plus":
-		this_pos = [pos_choices[random.randint(0, 4)]]
-	elif pos == "all":
-		this_pos = pos_choices[:5]
-	else:
-		this_pos = [pos]
-
-	# Supersample the image
-	big_img = img.resize([ss * x for x in img.size])
-
-	vs, v_total = 0, len(this_pos) * len(this_color)
-		
-	for p in this_pos:
-		for suff, c in enumerate(this_color):
-			vs += 1
-
-			# Use white logo is background not white
-			this_wm = wm_color if c == (255, 255, 255) else wm_white
+		# Choose a color for the 'random' case
+		if args["color"] == "random":
+			color_list = [random.choice(list(color_choices.values()))]
 			
-			# No need for color suffix if only one color
-			if len(this_color) == 1:
-				suff = ""    
-				
-			# Resize watermark only if it doesn't exceed its own resolution
-			if wm_ratio > ratio:
-				this_w = int(wW * math.sqrt(ratio / wm_ratio))
-				this_h = int(wH * math.sqrt(ratio / wm_ratio))
-				this_wm = this_wm.resize((this_w, this_h), Image.ANTIALIAS)
+		# Randomize position if asked
+		pos_list = get_pos_list(pos)
 
-			# Export the image
-			print(proc_str.format(count, total, vs, v_total, invalid), end="\r")
-			export_image(img, big_img.copy(), ss, this_wm, p, c, dist, file_out, circle=circle, radius=radius, suff=suff)
+		# Watermark picture
+		watermark_image(img, file_out, logos, ratios, pos_list, color_list, draw_circle=not args["no_circle"], center_circle=args["center_circle"])
 
-print()
-print(end_str.format(total) + (inv_str.format(invalid) if invalid else ""))
+	print("Done")
+
+	# BRING BACK INVALID TEXT + PUT IN INVALID FOLDER
+
+	# COMMENT EVERYTHING
